9a0ac9ba946a1d8d8cc19e65e24896bfae9a111e
Priyansh Jain <priyanshjain301@gmail.com>
2023-09-07 01:44:53 +0530

    Bottom navigation created

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryColor = Color(0xFFFFFFFF);
+  static const Color secondaryColor = Color(0xFF717075);
+  static const Color tertiaryColor = Color(0xFFBBBDC1);
 }
