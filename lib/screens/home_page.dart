b9c38f9932b9a11515e33388d1bf164451d294d3
Priyansh Jain <priyanshjain301@gmail.com>
2023-09-10 16:33:53 +0530

    empty message

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -71,141 +71,143 @@ class _HomePage extends State<HomePage> {
         onTap: () {
           FocusScope.of(context).requestFocus(FocusNode());
         },
-        child: _showProgress
-            ? const Center(
-                child:
-                    CircularProgressIndicator(color: AppColors.secondaryColor),
-              )
-            : SingleChildScrollView(
-                controller: _listScrollController,
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(15, 30, 15, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              RichText(
-                                text: TextSpan(
-                                  style: AppFonts.titleFont(),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                      text: 'Almanac-Of',
-                                      style: AppFonts.titleFont(
-                                          fontColor: AppColors.tertiaryColor),
-                                    ),
-                                    const TextSpan(
-                                      text: '-Wisdom',
-                                    ),
-                                  ],
-                                ),
+        child: SingleChildScrollView(
+          controller: _listScrollController,
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(15, 30, 15, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: AppFonts.titleFont(),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Almanac-Of',
+                                style: AppFonts.titleFont(
+                                    fontColor: AppColors.tertiaryColor),
                               ),
-                              const SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Build your ',
-                                    style: AppFonts.subtitleFont(),
-                                  ),
-                                  DefaultTextStyle(
-                                    style: AppFonts.subtitleFont(
-                                      fontColor: AppColors.accentColor,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    child: AnimatedTextKit(
-                                      repeatForever: true,
-                                      animatedTexts: [
-                                        RotateAnimatedText('Wealth',
-                                            transitionHeight:
-                                                _transitionHeight),
-                                        RotateAnimatedText('Happiness',
-                                            transitionHeight:
-                                                _transitionHeight),
-                                        RotateAnimatedText('Judgement',
-                                            transitionHeight:
-                                                _transitionHeight),
-                                        RotateAnimatedText('Wisdom',
-                                            transitionHeight:
-                                                _transitionHeight),
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                              const TextSpan(
+                                text: '-Wisdom',
                               ),
                             ],
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 25),
-                      Row(
-                        children: [
-                          const Icon(IconlyLight.search),
-                          const SizedBox(width: 10),
-                          Expanded(
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(50),
-                                ),
-                                color: Color.fromARGB(222, 245, 245, 249),
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Build your ',
+                              style: AppFonts.subtitleFont(),
+                            ),
+                            DefaultTextStyle(
+                              style: AppFonts.subtitleFont(
+                                fontColor: AppColors.accentColor,
+                                fontWeight: FontWeight.bold,
                               ),
-                              margin: const EdgeInsets.only(right: 20),
-                              padding: const EdgeInsets.only(
-                                  left: 16, right: 14, top: 18, bottom: 10),
-                              child: TextField(
-                                controller: _searchTextEditingController,
-                                style: AppFonts.subtitleFont(
-                                    fontColor: AppColors.negativeColor,
-                                    fontWeight: FontWeight.bold),
-                                decoration: const InputDecoration(
-                                  constraints: BoxConstraints(maxHeight: 28),
-                                  hintText:
-                                      'Try searching "How to build wealth"',
-                                  border: InputBorder.none,
-                                ),
-                                onChanged: (value) {
-                                  _fetchPosts(
-                                      searchQuery: value, isSearching: true);
-
-                                  _listScrollController.animateTo(
-                                    _listScrollController
-                                        .position.minScrollExtent,
-                                    duration: const Duration(milliseconds: 500),
-                                    curve: Curves.fastOutSlowIn,
-                                  );
-                                },
+                              child: AnimatedTextKit(
+                                repeatForever: true,
+                                animatedTexts: [
+                                  RotateAnimatedText('Wealth',
+                                      transitionHeight: _transitionHeight),
+                                  RotateAnimatedText('Happiness',
+                                      transitionHeight: _transitionHeight),
+                                  RotateAnimatedText('Judgement',
+                                      transitionHeight: _transitionHeight),
+                                  RotateAnimatedText('Wisdom',
+                                      transitionHeight: _transitionHeight),
+                                ],
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 35),
-                      Text('For You', style: AppFonts.headingFont()),
-                      const SizedBox(height: 10),
-                      ListView.separated(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: _visibleItems.length,
-                        itemBuilder: (context, index) {
-                          return CustomWidget.postListTile(
-                             _visibleItems[index], context);
-                        },
-                        separatorBuilder: (context, index) {
-                          return const Divider(
-                            height: 1,
-                            color: Color.fromARGB(255, 245, 245, 249),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
+                const SizedBox(height: 25),
+                Row(
+                  children: [
+                    const Icon(IconlyLight.search),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(50),
+                          ),
+                          color: Color.fromARGB(222, 245, 245, 249),
+                        ),
+                        margin: const EdgeInsets.only(right: 20),
+                        padding: const EdgeInsets.only(
+                            left: 16, right: 14, top: 18, bottom: 10),
+                        child: TextField(
+                          controller: _searchTextEditingController,
+                          style: AppFonts.subtitleFont(
+                              fontColor: AppColors.negativeColor,
+                              fontWeight: FontWeight.bold),
+                          decoration: const InputDecoration(
+                            constraints: BoxConstraints(maxHeight: 28),
+                            hintText: 'Try searching "How to build wealth"',
+                            border: InputBorder.none,
+                          ),
+                          onChanged: (value) {
+                            _fetchPosts(searchQuery: value, isSearching: true);
+
+                            _listScrollController.animateTo(
+                              _listScrollController.position.minScrollExtent,
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.fastOutSlowIn,
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 35),
+                Text('For You', style: AppFonts.headingFont()),
+                const SizedBox(height: 10),
+                _showProgress
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                            color: AppColors.secondaryColor),
+                      )
+                    : _visibleItems.isEmpty
+                        ? Center(
+                            child: Text(
+                              'Nothing found!\nTry searching for something else.',
+                              style: AppFonts.subtitleFont(
+                                  fontColor: AppColors.negativeColor),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        : ListView.separated(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: _visibleItems.length,
+                            itemBuilder: (context, index) {
+                              return CustomWidget.postListTile(
+                                  _visibleItems[index], context);
+                            },
+                            separatorBuilder: (context, index) {
+                              return const Divider(
+                                height: 1,
+                                color: Color.fromARGB(255, 245, 245, 249),
+                              );
+                            },
+                          ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
